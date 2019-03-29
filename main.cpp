917ae11d38673d91b3201d44850edf0e34659d95
miguelyurivilca <miguel_y_r@hotmail.com>
2019-03-29 10:39:50 -0500

    completed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 
 int get_num_cycles(int n)
 {
@@ -28,24 +28,30 @@ int main(int argc, char **argv) {
     int j;
     int max_cycle;
 
-    std::vector<std::pair<int, int> > lines;
+    std::vector<std::pair<std::pair<int, int>, bool> > lines;
 
     while (std::cin >> i >> j)
     {
-        lines.push_back(std::make_pair(i, j));
+        if (i < j)
+            lines.push_back(std::make_pair(std::make_pair(i, j), false));
+        else
+            lines.push_back(std::make_pair(std::make_pair(j, i), true));
     }
 
     for (auto i : lines)
     {
         max_cycle = 0;
-        for (int j = i.first; j <= i.second; ++j)
+        for (int j = i.first.first; j <= i.first.second; ++j)
         {
             if (get_num_cycles(j) > max_cycle)
             {
                 max_cycle = get_num_cycles(j);
             }
         }
-        std::cout << i.first << ' ' << i.second << ' ' << max_cycle << '\n';
+        if (i.second == false)
+            std::cout << i.first.first << ' ' << i.first.second << ' ' << max_cycle << '\n';
+        else
+            std::cout << i.first.second << ' ' << i.first.first << ' ' << max_cycle << '\n';
     }
 
     return 0;
