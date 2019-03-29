71c87c52fc20e7c183a1e8b8cd70fee492a7635e
miguelyurivilca <miguel_y_r@hotmail.com>
2019-03-29 10:12:44 -0500

    working program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+int get_num_cycles(int n)
+{
+    int result;
 
-int main(int argc, char *argv[]) {
+    result = 0;
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+            result++;
+        }
+        else
+        {
+            n = 3 * n + 1;
+            result++;
+        }
+    }
+    return ++result;
+}
+
+int main(int argc, char **argv) {
+
+    int i;
+    int j;
+    int max_cycle;
+
+    std::vector<std::pair<int, int> > lines;
+
+    while (std::cin >> i >> j)
+    {
+        lines.push_back(std::make_pair(i, j));
+    }
+
+    for (auto i : lines)
+    {
+        max_cycle = 0;
+        for (int j = i.first; j <= i.second; ++j)
+        {
+            if (get_num_cycles(j) > max_cycle)
+            {
+                max_cycle = get_num_cycles(j);
+            }
+        }
+        std::cout << i.first << ' ' << i.second << ' ' << max_cycle << '\n';
+    }
+
     return 0;
 }
